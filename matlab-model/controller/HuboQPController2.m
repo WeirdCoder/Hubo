b8a1663dcdc8698cba2cf61d469764c3742f6c95
Phil <pcherner@mit.edu>
2014-08-11 13:30:14 -0400

    Updated QP

---
+++ matlab-model/controller/HuboQPController2.m
@@ -0,0 +1,43 @@
+classdef HuboQPController2 < MIMODrakeSystem
+    %Non Hubo-specific module
+    % @param input_frames given cells of frames as input{}.
+    % @retval joined output from the input.
+    
+    %The module is not yet tested.
+    properties
+        dt
+        A
+        B
+        H
+        f
+        ub
+        lb
+    end
+    methods
+        function obj = HuboQPController2(dt)
+            input_frame = MultiCoordinateFrame({CoordinateFrame('x_des',6,'x'),CoordinateFrame('x_curr',6,'x')});
+            output_frame = MultiCoordinateFrame.constructFrame({CoordinateFrame('xdot_des',3,'x')});
+            obj@MIMODrakeSystem(0,0,input_frame,output_frame,true, true);
+            obj.dt = dt;
+            load hubo_catching3.mat
+            obj.A = A;
+            obj.B = B;
+            obj.H = H;
+            obj.f = f;
+            obj.ub = ub;
+            obj.lb = lb;
+        end
+        function xdot_des=mimoOutput(obj,~,~,varargin)        
+            x_ball = varargin{1};
+            x_hand = varargin{2};
+            obj.B(1:6) = x_hand;
+            obj.B(7:12) = x_ball;
+            options.display = 'off';
+            qpProblem = QuadraticProgram(obj.H,obj.f,[],[],obj.A,obj.B,[],[]);
+            [x,fval,exitflag,output] = qpProblem.solve();
+            xdot_des = x(16:18);
+            xdot2 = x(28:30);
+            xdot3 = x(40:52);
+        end
+    end
+end
